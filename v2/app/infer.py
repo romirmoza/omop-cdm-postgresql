8a571032be754603f2315bdf9f4e84fa3c96f008
Romir Moza <romir.moza@gmail.com>
2020-01-28 12:28:27 -0500

    added memory compression of the dataframe

--- v2/app/infer.py
+++ v2/app/infer.py
@@ -9,6 +9,51 @@ import xgboost as xgb
 
 ROOT = "/"
 
+def reduce_mem_usage(df):
+    start_mem = df.memory_usage().sum() / 1024**2
+    print(str(pd.datetime.now())+"::"+os.path.realpath(__file__)+"::"+'Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+        if col_type != object and col_type :
+                c_min = df[col].min()
+                c_max = df[col].max()
+                c_unique = len(df[col].unique())
+
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.uint8).min and c_max < np.iinfo(np.uint8).max:
+                        df[col] = df[col].astype(np.uint8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.uint16).min and c_max < np.iinfo(np.uint16).max:
+                        df[col] = df[col].astype(np.uint16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.uint32).min and c_max < np.iinfo(np.uint32).max:
+                        df[col] = df[col].astype(np.uint32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+                    elif c_min > np.iinfo(np.uint64).min and c_max < np.iinfo(np.uint64).max:
+                        df[col] = df[col].astype(np.uint64)
+                else:
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float16)
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float64)
+
+                if c_min == 0.0 and c_max == 1.0 and c_unique == 2:
+                    df[col] = df[col].astype(bool)
+                    pass
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print(str(pd.datetime.now())+"::"+os.path.realpath(__file__)+"::"+'Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print(str(pd.datetime.now())+"::"+os.path.realpath(__file__)+"::"+'Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    return df
+
 class OmopParser(object):
 
     def __init__(self):
@@ -20,6 +65,12 @@ class OmopParser(object):
     def load_data(self, test_filename, train_filename):
         print(str(pd.datetime.now())+"::"+os.path.realpath(__file__)+"::"+"Infer load data start", flush = True)
         test = pd.read_csv(test_filename,low_memory = False)
+        test = test.fillna(0)
+        test.old = test.old.astype(int)
+        for c in test.columns[test.columns.str.startswith('days')]:
+            test[c] = test[c].astype(int)
+        test = reduce_mem_usage(test)
+
         from sklearn.preprocessing import LabelEncoder
         label_encoder = LabelEncoder()
         label_encoder = label_encoder.fit(test.race_concept_name)
